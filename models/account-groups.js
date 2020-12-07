c62ddd03caf13129243fe072867b5fdbac18dd83
Leko <rexx0520@gmail.com>
2020-12-07 21:55:14 +0800

    修正 tableName 爆炸的問題

--- models/account-groups.js
+++ models/account-groups.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const db = require("./db")
 
-const tableName = "account-groups"
+const tableName = "\"account-groups\""
 
 module.exports = {
     items: function (req, callback) {

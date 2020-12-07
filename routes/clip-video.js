b949df664a91acac36cf7dda7281d4146daa04f5
Leko <rexx0520@gmail.com>
2020-12-07 22:03:12 +0800

    料料說不要 404 要 []

--- routes/clip-video.js
+++ routes/clip-video.js
@@ -36,11 +36,7 @@ router.route('/')
                 return console.error(err);
             }
 
-            // 沒有找到指定的資源
-            if (!results.length) {
-                res.sendStatus(404);
-                return;
-            }
+
 
             res.json(results);
         });
